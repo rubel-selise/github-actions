1945589342242fea95893ba44b1451e1a170defa
Rubel Mia <mdrobin.eng@gmail.com>
2023-03-07 00:18:42 +0600

    Catalog service added

---
+++ Catalog/Repositories/IItemsRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task<Item> GetItemAsync(Guid id);
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
+    }
+}
